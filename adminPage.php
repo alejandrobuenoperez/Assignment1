086e94d26b5d1e8c4f0b7e4ea6e5bbfa37634df7
alejandrobuenoperez <alexbuenoperez@gmail.com>
2022-09-22 14:22:51 +0200

    adminPage added

---
+++ adminPage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Page</title>
+    </head>
+    <body>
+        <form action='' method='post'>
+            <label>Select a file: </label>
+            <input type='file'accept='.json'><br>
+            <input type='submit' value='upload'>
+        </form>
+    </body>
+</html>
