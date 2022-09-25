eafd7c0de8e6dc515fe6e191b3487d700ece9c14
M4RCKIT0S <marcosicardochicote15@gmail.com>
2022-09-25 17:51:03 +0200

    Login with db

---
+++ dbConnect.php
@@ -0,0 +1,11 @@
+<?php
+    define('DB_SERVER','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','Hipolito.88');
+    define('DB_NAME','Assignment3');
+
+    $connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+    if($connection===false){
+        die('Error connecting to database: ' . mysqli_connect_error());
+    }
+?>
--- logIn.php
+++ logIn.php
@@ -5,17 +5,38 @@
         header("location: adminPage.php");
         exit;
     }
-    $username =  "";
+
+    require_once "dbConnect.php";
+    $username = $password = $userId = "";
     $loginError = "";
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         if(empty(trim($_POST["username"])) || empty(trim($_POST["password"]))){
             $loginError = "Username or password not defined";
         }else{
-            $username=$_POST["username"];
-            session_start();
-            $_SESSION["loggedin"] = true;
-            $_SESSION["username"] = $username;
-            header("location: adminPage.php"); 
+            $sqlQuery = "SELECT userId,Username, Password FROM Users WHERE Username = ?;";
+            $statement = mysqli_prepare($connection,$sqlQuery);
+            mysqli_stmt_bind_param($statement, "s", $usernameParam);
+            $usernameParam = trim($_POST["username"]);
+            $passwordParam = trim($_POST["password"]);
+            mysqli_stmt_execute($statement);
+            mysqli_stmt_store_result($statement);
+            if(mysqli_stmt_num_rows($statement) == 1){ 
+                mysqli_stmt_bind_result($statement, $userId, $username, $password);
+                mysqli_stmt_fetch($statement);
+                if($passwordParam===$password){
+                    session_start();
+                    $_SESSION["userId"] = $userId;
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["username"] = $username;
+                    header("location: adminPage.php"); 
+                }else{
+                    $loginError = "Invalid user or password.";
+                }
+            }else{
+                $loginError = "Not user found.";
+            }
+            mysqli_stmt_close($statement);
+            mysqli_close($connection);
         }
     }
 ?>
