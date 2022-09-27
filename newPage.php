753b027b385763124283bddb210f439dd18e87f5
alejandrobuenoperez <alexbuenoperez@gmail.com>
2022-09-28 00:45:31 +0200

    newPage added

--- newPage.php
+++ newPage.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <head>
-    
+    <title>New Page</title>
+    <link rel="stylesheet" href="StylesNewPage.css" type="text/css">
 </head>
 <body>
+    <div class="container">
+        <h2 class="title">Titulo de la noticia</h2>
+        <img class="image" src="Images/asteroid-impact.png">
+        <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
 </body>
 </html>
